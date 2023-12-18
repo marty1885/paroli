c1364164a978ff81da80e81248cd03dbcb1e1e2b
marty1885 <marty188586@gmail.com>
2023-12-18 17:11:41 +0000

    initial commit

---
+++ pipercore/pch.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+
+
+#include <spdlog/spdlog.h>
+#include <fmt/core.h>
+
+#include <nlohmann/json.hpp>
